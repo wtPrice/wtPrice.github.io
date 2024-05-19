9409c2e946540d906b5de18576839f0cb0725639
DundaD <danielfdunda@gmail.com>
2024-05-19 05:25:37 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
 import { registerSW } from 'workbox-service-worker';
 
 registerSW();
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'styles.css',
+        'script.js',
+        'images/image1.jpg',
+        'images/image2.jpg'
+      ]);
+    })
+  );
+});
