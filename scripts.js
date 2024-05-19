2c47c737f18a5d3506653c44e9e070a11bbc2983
DundaD <danielfdunda@gmail.com>
2024-05-19 05:57:43 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
-const form = document.getElementById('form');
-const generateButton = document.getElementById('generate-button');
-const imageContainer = document.getElementById('image-container');
+const generateButtons = document.querySelectorAll('#generate-button-1, #generate-button-2, #generate-button-3, #generate-button-4');
+const imageContainers = document.querySelectorAll('#image-container-1, #image-container-2, #image-container-3, #image-container-4');
 
-generateButton.addEventListener('click', async () => {
-  const title = document.getElementById('title').value;
-  const intro = document.getElementById('intro').value;
-  const body = document.getElementById('body').value;
-  const conclusion = document.getElementById('conclusion').value;
+generateButtons.forEach((button, index) => {
+  button.addEventListener('click', async () => {
+    const title = document.getElementById('title').value;
+    const intro = document.getElementById('intro').value;
+    const body = document.getElementById('body').value;
+    const conclusion = document.getElementById('conclusion').value;
 
-  // Make an API call to the Pixels API to generate the images
-  const response = await fetch(`https://api.zPhMQms3thyBjnQnph7jxMBUSau1KlrRKjHOpeOfwkKtT0NXzADh0L8D/pixels?title=${title}&intro=${intro}&body=${body}&conclusion=${conclusion}`);
-  const imageData = await response.json();
+    // Make an API call to the Pixels API to generate the images
+    const response = await fetch(`https://api.zPhMQms3thyBjnQnph7jxMBUSau1KlrRKjHOpeOfwkKtT0NXzADh0L8D/pixels?title=${title}&intro=${intro}&body=${body}&conclusion=${conclusion}`);
+    const imageData = await response.json();
 
-  // Use the Ideogram wrapper to generate the visual representation of the pixel count
-  const ideogram = new Ideogram(imageData.pixel_count);
-  const visualRepresentation = ideogram.generateVisualRepresentation();
+    // Use the Ideogram wrapper to generate the visual representation of the pixel count
+    const ideogram = new Ideogram(imageData.pixel_count);
+    const visualRepresentation = ideogram.generateVisualRepresentation();
 
-  // Update the HTML element with the visual representation
-  imageContainer.innerHTML = visualRepresentation;
+    // Update the HTML element with the visual representation
+    imageContainers[index].innerHTML = visualRepresentation;
+  });
 });
