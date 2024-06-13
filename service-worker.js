4b96101cea368ac4ea1401ebffed262a9c4f0afa
DundaD <danielfdunda@gmail.com>
2024-06-12 17:44:01 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,49 @@
+const CACHE_NAME = 'tree-estimate-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/icons/height.png',
+  '/icons/canopy_radius.png',
+  '/icons/canopy_height.png',
+  '/icons/density.png',
+  '/icons/basic_trim.png',
+  '/icons/moderate_trim.png',
+  '/icons/heavy_trim.png',
+  '/icons/removal.png'
+];
 
+// Install the service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update the service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
